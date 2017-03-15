35d5c30308889664a48cb262b8f0dd5a4ae4420f
Jorge Pinto <jmigpin@gmail.com>
2017-03-15 21:33:23 +0000

    Improve findshortcut.

--- edit/cmdutil/findshortcut.go
+++ edit/cmdutil/findshortcut.go
@@ -1,19 +1,30 @@
 package cmdutil
 
 import (
+	"strings"
+
 	"github.com/jmigpin/editor/drawutil"
 	"github.com/jmigpin/editor/edit/toolbardata"
 	"github.com/jmigpin/editor/ui"
 	"github.com/jmigpin/editor/ui/tautil"
 )
 
-// Search for the find command in the toolbar and warps the pointer to it. Adds the command to the toolbar if not present.
+// Search/add the toolbar find command and warps the pointer to it.
 func FindShortcut(ed Editori, row *ui.Row) {
 	// check if there is a selection in the textarea
 	searchStr := ""
 	if row.TextArea.SelectionOn() {
 		a, b := tautil.SelectionStringIndexes(row.TextArea)
 		searchStr = row.TextArea.Str()[a:b]
+		// if more then one line - no search string
+		for _, ru := range searchStr {
+			if ru == '\n' {
+				searchStr = "" //searchStr[:i]
+				break
+			}
+		}
+
+		searchStr = strings.TrimSpace(searchStr)
 	}
 
 	// find cmd in toolbar string
@@ -28,32 +39,46 @@ func FindShortcut(ed Editori, row *ui.Row) {
 			break
 		}
 	}
-	if !found || searchStr != "" {
+
+	if !found {
 		// insert find cmd
-		ta.EditInsert(len(ta.Str()), " | Find "+searchStr)
-		ta.EditDone()
+		ta.EditOpen()
+		ta.EditInsert(len(ta.Str()), " | Find ")
 		ta.SetSelectionOn(false)
-		ta.SetCursorIndex(len(ta.Str()))
-	} else if found {
-		if len(part.Args) == 1 {
-			// no other args
-			a := part.Start + part.Args[0].End
-			b := part.End
-			if a == b {
-				// insert a space
-				ta.EditInsert(a, " ")
-				ta.EditDone()
-			}
-			ta.SetSelectionOn(false)
-			ta.SetCursorIndex(a + 1)
-		} else {
-			// select arg string
-			a := part.Start + part.Args[0].End + 1
-			b := part.Start + part.Args[len(part.Args)-1].End
+		a := len(ta.Str())
+		if searchStr != "" {
 			ta.SetSelectionOn(true)
 			ta.SetSelectionIndex(a)
-			ta.SetCursorIndex(b)
+			ta.EditInsert(a, searchStr)
+			a = len(ta.Str())
 		}
+		ta.SetCursorIndex(a + len(searchStr))
+		ta.EditClose()
+	} else {
+		ta.EditOpen()
+		// select current find cmd string
+		a := part.Start + part.Args[0].End
+		b := part.End
+		if a == b {
+			ta.EditInsert(a, " ")
+			a++
+			b++
+		} else {
+			a++
+			ta.SetSelectionOn(true)
+			ta.SetSelectionIndex(a)
+		}
+		ta.SetCursorIndex(b)
+
+		// replace current find cmd string with search str
+		if searchStr != "" {
+			ta.EditDelete(a, b)
+			ta.EditInsert(a, searchStr)
+			ta.SetSelectionOn(true)
+			ta.SetSelectionIndex(a)
+			ta.SetCursorIndex(a + len(searchStr))
+		}
+		ta.EditClose()
 	}
 
 	// warp pointer to toolbar close to "Find " text cmd to be able to click for run
