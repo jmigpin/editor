02207c091969ffa9e1664d555d36bf8ed75ed2f4
Jorge Pinto <jmigpin@gmail.com>
2017-03-15 21:33:08 +0000

    Catch panic output to a file to debug crashes.

--- editor.go
+++ editor.go
@@ -2,16 +2,37 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
+	"syscall"
 
 	"github.com/jmigpin/editor/edit"
 )
 
 func main() {
 	log.SetFlags(log.Llongfile)
-	_, err := edit.NewEditor()
+
+	// redirect stderr to log panics
+	f, err := os.Create("/home/jorge/editor_stderr.txt")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
+		return
+	}
+	defer f.Close()
+
+	// panic to file
+	err = syscall.Dup2(int(f.Fd()), int(os.Stderr.Fd()))
+	if err != nil {
+		log.Fatalf("Failed to redirect stderr to file: %v", err)
+		return
+	}
+
+	//mw := io.MultiWriter(f, os.Stdout)
+	//log.SetOutput(mw)
+	log.SetOutput(os.Stdout)
+
+	_, err = edit.NewEditor()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
