a8e1c323a7993e6eb5a216edb31b208c17a37a17
Jorge Pinto <jmigpin@gmail.com>
2017-03-18 11:00:15 +0000

    Set font depending test to depend on goregular instead.

--- drawutil/glyphbuf_test.go
+++ drawutil/glyphbuf_test.go
@@ -1,16 +1,15 @@
 package drawutil
 
 import (
-	"io/ioutil"
 	"testing"
 
 	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/goregular"
 	"golang.org/x/image/math/fixed"
 
 	"github.com/golang/freetype/truetype"
 )
 
-var font0Filename = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
 var loremText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
 
 var dpi = float64(72)
@@ -19,15 +18,11 @@ var scale = fixed.Int26_6(0.5 + (size * dpi * 64 / 72))
 var font0 = initFont()
 
 func initFont() *truetype.Font {
-	bytes, err := ioutil.ReadFile(font0Filename)
+	font, err := truetype.Parse(goregular.TTF)
 	if err != nil {
 		panic(err)
 	}
-	f, err := truetype.Parse(bytes)
-	if err != nil {
-		panic(err)
-	}
-	return f
+	return font
 }
 
 func BenchmarkF0(b *testing.B) {
