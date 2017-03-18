4a819c7a94ebc4cb16094fa643ea7f16c343edd9
Jorge Pinto <jmigpin@gmail.com>
2017-03-18 11:37:22 +0000

    Strincache calcrunedata test.

---
+++ drawutil/stringcache_test.go
@@ -0,0 +1,28 @@
+package drawutil
+
+import (
+	"testing"
+
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font/gofont/goregular"
+)
+
+var str = ""
+
+func init() {
+	for i := 0; i < 100000; i++ {
+		str += "abcde"
+	}
+}
+
+func BenchmarkCalcRuneData0(t *testing.B) {
+	font, err := truetype.Parse(goregular.TTF)
+	if err != nil {
+		return
+	}
+	opt := &truetype.Options{Size: 13}
+	face := NewFace(font, opt)
+	sc := NewStringCache(face)
+	w := 50
+	sc.CalcRuneData(str, w)
+}
