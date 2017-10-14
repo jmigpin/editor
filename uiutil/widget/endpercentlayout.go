cd2d14cca2902d7c1dd702835dfb0f47e7c9ede1
Jorge Pinto <jmigpin@gmail.com>
2017-10-14 18:20:38 +0100

    Improve endpercentlayout with resizes and hot swaps.

--- uiutil/widget/endpercentlayout.go
+++ uiutil/widget/endpercentlayout.go
@@ -17,7 +17,48 @@ func (epl *EndPercentLayout) lazyInit() {
 		epl.endPercents = make(map[Node]float64)
 	}
 }
+
+func (epl *EndPercentLayout) PushBack(parent, n Node) {
+	epl.EmbedNode.PushBack(parent, n)
+
+	epl.lazyInit()
+
+	// share with last sibling
+	start := 0.0
+	end := 1.0
+	if n.Prev() != nil {
+		if n.Prev().Prev() != nil {
+			start = epl.endPercents[n.Prev().Prev()]
+		}
+		epl.endPercents[n.Prev()] = start + (end-start)/2
+	}
+	epl.endPercents[n] = end
+}
+func (epl *EndPercentLayout) InsertBefore(parent, n, mark Node) {
+	epl.EmbedNode.InsertBefore(parent, n, mark)
+
+	epl.lazyInit()
+
+	// insert between siblings end percents
+	start := 0.0
+	end := 1.0
+	if n.Prev() != nil {
+		if n.Prev().Prev() != nil {
+			start = epl.endPercents[n.Prev().Prev()]
+		}
+		end = epl.endPercents[n.Prev()]
+		epl.endPercents[n.Prev()] = start + (end-start)/2
+	}
+	epl.endPercents[n] = end
+}
+
 func (epl *EndPercentLayout) Remove(n Node) {
+	// have the previous node keep the space
+	if n.Prev() != nil {
+		u := epl.endPercents[n]
+		epl.SetChildEndPercent(n.Prev(), u)
+	}
+
 	delete(epl.endPercents, n)
 	epl.EmbedNode.Remove(n)
 }
@@ -57,51 +98,7 @@ func (epl *EndPercentLayout) CalcChildsBounds() {
 	bounds := epl.Bounds()
 	abounds := xya.Rectangle(&bounds)
 
-	// define end percents missing
-	for _, child := range childs {
-		ep, ok := epl.endPercents[child]
-		if ok {
-			continue
-		}
-
-		// previous end percent
-		prevEP := 0.0
-		if child.Prev() != nil {
-			prevEP, _ = epl.endPercents[child.Prev()]
-		}
-
-		// next end percent
-		nextEP := 1.0
-		n := 1
-		for c := child.Next(); c != nil; c = c.Next() {
-			n++
-			ep, ok = epl.endPercents[c]
-			if ok {
-				nextEP = ep
-				break
-			}
-		}
-
-		// share for the n childs not defined
-		share := (nextEP - prevEP) / float64(n)
-
-		// appending to a child at endpercent 1.0 - share space
-		if share == 0 && child == epl.LastChild() && child.Prev() != nil {
-			start := 0.0
-			if child.Prev().Prev() != nil {
-				start, _ = epl.endPercents[child.Prev().Prev()]
-			}
-			end, _ := epl.endPercents[child.Prev()]
-			share = (end - start) / 2
-			prevEP = start + share
-			epl.endPercents[child.Prev()] = prevEP
-		}
-
-		// set share
-		epl.endPercents[child] = prevEP + share
-	}
-
-	// set sizes and end percents
+	// set sizes
 	cxs := abounds.Min.X
 	for _, child := range childs {
 		ep, _ := epl.endPercents[child]
@@ -125,5 +122,72 @@ func (epl *EndPercentLayout) CalcChildsBounds() {
 		r3 := r2.Intersect(epl.Bounds())
 		child.SetBounds(&r3)
 		child.CalcChildsBounds()
+	}
+}
+
+func (epl *EndPercentLayout) ResizeEndPercent(node Node, ep, min float64) {
+	// limit to siblings bounds
+	start := 0.0
+	if node.Prev() != nil {
+		if node.Prev().Prev() != nil {
+			start = epl.endPercents[node.Prev().Prev()]
+		}
+	}
+	end := epl.endPercents[node]
+	if ep < start+min {
+		ep = start + min
+	}
+	if ep > end-min {
+		ep = end - min
+	}
+
+	// if there is no other option, squash it
+	if ep < start+min {
+		ep = start + (end-start)/2
+	}
+
+	// resize
+	if node.Prev() != nil {
+		epl.SetChildEndPercent(node.Prev(), ep)
+	}
+}
+
+func (epl *EndPercentLayout) AttemptToSwap(parent, node Node, ep, min float64) {
+	n := node
+	if n.Prev() != nil && n.Prev().Prev() != nil {
+		start := epl.endPercents[n.Prev().Prev()]
+		if ep < start-min {
+			prev := node.Prev()
+			epl.Remove(node)
+			epl.InsertBefore(parent, node, prev)
+			epl.ResizeEndPercent(n, ep, min)
+		}
+	}
+	if n.Next() != nil {
+		start := epl.endPercents[n]
+		if ep > start+min {
+			u := n.Next()
+			epl.Remove(u)
+			epl.InsertBefore(parent, u, n)
+			if u.Prev() != nil {
+				epl.SetChildEndPercent(u.Prev(), start)
+			}
+			epl.ResizeEndPercent(n, ep, min)
+		}
+	}
+
+}
+
+func (epl *EndPercentLayout) MaximizeEndPercentNode(node Node, min float64) {
+	childs := epl.Childs()
+	n := len(childs)
+	ep := 0.0
+	for i, c := range childs {
+		if c == node {
+			ep = 1.0 - min*float64(n-(i+1))
+		} else {
+			ep += min
+		}
+		epl.SetChildEndPercent(c, ep)
 	}
 }
