a6b9f69d5e01703518ff51c41f7b5a0e30a23f52
Jorge Pinto <jmigpin@gmail.com>
2017-10-17 02:49:35 +0100

    Bubble up events in the parent chain for the press node.

--- uiutil/widget/node.go
+++ uiutil/widget/node.go
@@ -99,49 +99,45 @@ var pressNode Node
 // Auto-locks all input events into the node on the tree that handles
 // a press event. The node is responsible for unlocking by handling
 // the release event. Handling means OnInputEvent() returns true.
-// TESTING: scrollarea, square, textarea
 func ApplyInputEventInBounds(
 	node Node,
 	ev interface{},
 	p image.Point,
 	isPressEvent func(ev interface{}) bool,
 	isReleaseEvent func(ev interface{}) bool,
-) bool {
+) {
 
 	// run events on press node only when present
 	if pressNode != nil {
+		pn := pressNode
+
 		handled := pressNode.OnInputEvent(ev, p)
-		if handled {
-			// unlock node
-			if isReleaseEvent(ev) {
-				//log.Printf("unlocking %p", pressNode)
-				pressNode = nil
-			}
-			return true
+		if handled && isReleaseEvent(ev) {
+			pressNode = nil // unlock node
 		}
-		return false
+
+		// call event up in the parent chain
+		for u := pn.Parent(); u != nil; u = u.Parent() {
+			_ = u.OnInputEvent(ev, p)
+		}
+
+		return
 	}
 
-	// reversed iteration for the possibility that later childs are drawn over
-	// hidden notes are inherently not iterated
+	// Reversed iteration for the possibility that later childs are drawn over.
+	// Hidden nodes are inherently not iterated.
 	for c := node.LastChild(); c != nil; c = c.Prev() {
 		if p.In(c.Bounds()) {
-			handled := ApplyInputEventInBounds(c, ev, p, isPressEvent, isReleaseEvent)
-			if handled {
-				return true
-			}
+			ApplyInputEventInBounds(c, ev, p, isPressEvent, isReleaseEvent)
 		}
 	}
 
 	handled := node.OnInputEvent(ev, p)
-	if handled {
-		// lock node
-		if isPressEvent(ev) {
-			//log.Printf("locking %p", node)
-			pressNode = node
+	if handled && isPressEvent(ev) {
+		if pressNode == nil { // only the first one locks
+			pressNode = node // lock node
 		}
 	}
-	return handled
 }
 
 type EmbedNode struct {
