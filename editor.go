bb5504ae38c03c8b1405f3115524235db25ae7fa
Jorge Pinto <jmigpin@gmail.com>
2017-10-25 00:34:07 +0100

    Switch wrapline newline rune.

--- editor.go
+++ editor.go
@@ -21,7 +21,7 @@ func main() {
 	dpiFlag := flag.Float64("dpi", 72, "monitor dots per inch")
 	scrollbarWidth := flag.Int("scrollbarwidth", 12, "textarea scrollbar width")
 	acmeColors := flag.Bool("acmecolors", false, "acme editor color theme")
-	wrapLineRune := flag.Int("wraplinerune", 8596, "code for wrap line rune")
+	wrapLineRune := flag.Int("wraplinerune", 8594, "code for wrap line rune")
 	tabWidth := flag.Int("tabwidth", 8, "")
 	scrollbarLeft := flag.Bool("scrollbarleft", true, "set scrollbars on the left side")
 	sessionName := flag.String("sessionname", "", "open existing session")
