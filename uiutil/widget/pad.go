e92a94530648e5b07551651283923d1839dc256b
Jorge Pinto <jmigpin@gmail.com>
2017-11-02 11:31:38 +0000

    Fix widget.pad draw out of bounds.

--- uiutil/widget/pad.go
+++ uiutil/widget/pad.go
@@ -28,17 +28,32 @@ func (p *Pad) Set(v int) {
 	p.Left = v
 }
 func (p *Pad) Measure(hint image.Point) image.Point {
-	hint.X -= p.Right + p.Left
-	hint.Y -= p.Top + p.Bottom
-	m := p.FirstChild().Measure(hint)
+	h := hint
+	h.X -= p.Right + p.Left
+	h.Y -= p.Top + p.Bottom
+	if h.X < 0 {
+		h.X = 0
+	}
+	if h.Y < 0 {
+		h.Y = 0
+	}
+	m := p.ShellEmbedNode.Measure(h)
 	m.X += p.Right + p.Left
 	m.Y += p.Top + p.Bottom
+	if m.X > hint.X {
+		m.X = hint.X
+	}
+	if m.Y > hint.Y {
+		m.Y = hint.Y
+	}
 	return m
 }
 func (p *Pad) CalcChildsBounds() {
-	u := p.Bounds()
+	b := p.Bounds()
+	u := b
 	u.Min = u.Min.Add(image.Point{p.Left, p.Top})
 	u.Max = u.Max.Sub(image.Point{p.Right, p.Bottom})
+	u = u.Intersect(b)
 	child := p.FirstChild()
 	child.SetBounds(&u)
 	child.CalcChildsBounds()
@@ -47,20 +62,25 @@ func (p *Pad) Paint() {
 	if p.Color == nil {
 		return
 	}
+	b := p.Bounds()
 	// top
-	u := p.Bounds()
+	u := b
 	u.Max.Y = u.Min.Y + p.Top
+	u = u.Intersect(b)
 	imageutil.FillRectangle(p.ctx.Image(), &u, *p.Color)
 	// bottom
-	u = p.Bounds()
+	u = b
 	u.Min.Y = u.Max.Y - p.Bottom
+	u = u.Intersect(b)
 	imageutil.FillRectangle(p.ctx.Image(), &u, *p.Color)
 	// right
-	u = p.Bounds()
+	u = b
 	u.Min.X = u.Max.X - p.Right
+	u = u.Intersect(b)
 	imageutil.FillRectangle(p.ctx.Image(), &u, *p.Color)
 	// left
-	u = p.Bounds()
+	u = b
 	u.Max.X = u.Min.X + p.Left
+	u = u.Intersect(b)
 	imageutil.FillRectangle(p.ctx.Image(), &u, *p.Color)
 }
