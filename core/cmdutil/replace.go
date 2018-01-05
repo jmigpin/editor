7cb0ed68973ef5207284263c5d2272a0874c417f
Jorge Pinto <jmigpin@gmail.com>
2018-01-05 07:28:35 +0000

    Fix replace to use unquoted string.

--- core/cmdutil/replace.go
+++ core/cmdutil/replace.go
@@ -14,6 +14,6 @@ func Replace(erow ERower, part *toolbardata.Part) {
 		erow.Ed().Error(err)
 		return
 	}
-	old, new := a[0].Str, a[1].Str
+	old, new := a[0].UnquotedStr(), a[1].UnquotedStr()
 	tautil.Replace(erow.Row().TextArea, old, new)
 }
