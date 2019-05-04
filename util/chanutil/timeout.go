c719ca565143c20233ca068f830e770186d293b5
Jorge Pinto <jmigpin@gmail.com>
2019-05-04 02:19:45 +0100

    util/chanutil/timeout: improve to keep track of non-returning functions.

--- util/chanutil/timeout.go
+++ util/chanutil/timeout.go
@@ -7,22 +7,61 @@ import (
 )
 
 // Run fn or return early with error after duration d. Note fn continues to run.
-func CallTimeout(ctx context.Context, d time.Duration, errStrPrefix string, fn func() error) error {
+func CallTimeout(ctx context.Context, d time.Duration, msgPrefix string, asyncErrors chan<- error, fn func() error) error {
 	t := time.NewTicker(d)
 	defer t.Stop()
 
-	ch := make(chan error)
+	// keep track if fn ever returns
+	fnDone := make(chan interface{})
+	if asyncErrors != nil {
+		go waitForDone(fnDone, asyncErrors, msgPrefix)
+	}
+
+	// run fn in go routine
+	ch := make(chan error, 1)
 	go func() {
-		ch <- fn()
+		defer close(fnDone)
+		err := fn() // goroutine leaks if fn never returns
+		select {
+		case <-ctx.Done(): // too late, send error async (else err is lost)
+			if asyncErrors != nil {
+				asyncErrors <- err
+			}
+		default: // don't block select
+			ch <- err // won't block due to size 1 in chan
+		}
 	}()
 
 	select {
 	case err := <-ch:
 		return err
 	case <-t.C:
-		return fmt.Errorf("call timeout: %v, %v", errStrPrefix, d)
+		return fmt.Errorf("call timeout: %v, %v", msgPrefix, d)
 	case <-ctx.Done():
 		return ctx.Err()
+	}
+}
+
+func waitForDone(done <-chan interface{}, asyncErrors chan<- error, msgPrefix string) {
+	start := time.Now()
+	t := time.NewTicker(10 * time.Second) // send err every x secs
+	defer t.Stop()
+	sentErr := false
+	for {
+		select {
+		case <-t.C:
+			sentErr = true
+			d := time.Now().Sub(start)
+			asyncErrors <- fmt.Errorf("waiting for fn (%s): %v", msgPrefix, d)
+		case <-done:
+			// send final error if it had sent errors before
+			if sentErr {
+				d := time.Now().Sub(start)
+				asyncErrors <- fmt.Errorf("fn returned (%s): %v", msgPrefix, d)
+			}
+
+			return
+		}
 	}
 }
 
