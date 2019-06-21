a5be1c69ae0ff73fed393fb5d50a4445c01ab48a
Jorge Pinto <jmigpin@gmail.com>
2019-06-21 19:16:21 +0100

    core/contentcmds: make gotodefinition_lsproto higher in priority.

--- core/contentcmds/init.go
+++ core/contentcmds/init.go
@@ -4,11 +4,9 @@ import "github.com/jmigpin/editor/core"
 
 func init() {
 	// order matters
+	core.ContentCmds.Append("gotodefinition_lsproto", GoToDefinitionLSProto)
 	core.ContentCmds.Append("gotodefinition", GoToDefinitionGolang)
 	core.ContentCmds.Append("openfilename", OpenFilename)
 	core.ContentCmds.Append("opensession", OpenSession)
 	core.ContentCmds.Append("openurl", OpenURL)
-
-	// NOTE: least priority since under development. Can be canceled with escape key.
-	core.ContentCmds.Append("gotodefinition_lsproto", GoToDefinitionLSProto)
 }
