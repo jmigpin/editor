abd2d3600b4fcf3892a2b10ccadfb4ac072759b4
Jorge Pinto <jmigpin@gmail.com>
2019-07-09 15:53:26 +0100

    core/contentcmds: use golang guru as main gotodefinition (still more stable then gopls).

--- core/contentcmds/init.go
+++ core/contentcmds/init.go
@@ -4,8 +4,8 @@ import "github.com/jmigpin/editor/core"
 
 func init() {
 	// order matters
-	core.ContentCmds.Append("gotodefinition_lsproto", GoToDefinitionLSProto)
 	core.ContentCmds.Append("gotodefinition", GoToDefinitionGolang)
+	core.ContentCmds.Append("gotodefinition_lsproto", GoToDefinitionLSProto)
 	core.ContentCmds.Append("openfilename", OpenFilename)
 	core.ContentCmds.Append("opensession", OpenSession)
 	core.ContentCmds.Append("openurl", OpenURL)
