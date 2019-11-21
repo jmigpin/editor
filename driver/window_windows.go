b9b028ee57de6aa00e45b3d73c730c13a3450d0e
Jorge Pinto <jmigpin@gmail.com>
2019-11-21 22:22:07 +0000

    driver/windriver: warn about needing to compile with -tags=xproto for windows.

--- driver/window_windows.go
+++ driver/window_windows.go
@@ -2,8 +2,11 @@
 
 package driver
 
-import "github.com/jmigpin/editor/driver/windriver"
+import "fmt"
+
+//import "github.com/jmigpin/editor/driver/windriver"
 
 func NewWindow() (Window, error) {
-	return windriver.NewWindow()
+	//return windriver.NewWindow()
+	return nil, fmt.Errorf("Please compile with '-tags=xproto' to compile the version with xserver support. The native version when released will not need this tag.")
 }
