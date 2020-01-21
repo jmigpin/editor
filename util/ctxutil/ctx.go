2822590461eccbec5b6e66866b61b13f49e5e6b0
Jorge Pinto <jmigpin@gmail.com>
2020-01-21 22:44:45 +0000

    util/ctxutil: add ctx implementation.

---
+++ util/ctxutil/ctx.go
@@ -0,0 +1,41 @@
+package ctxutil
+
+// This is a reference implementation. In many cases it works best to just copy this code and keep adding methods instead of embedding.
+
+type Ctx struct {
+	Parent *Ctx
+	// name/value (short names to avoid usage, still exporting it)
+	N string
+	V interface{}
+}
+
+func (ctx *Ctx) WithValue(name string, value interface{}) *Ctx {
+	return &Ctx{ctx, name, value}
+}
+
+func (ctx *Ctx) Value(name string) (interface{}, *Ctx) {
+	for c := ctx; c != nil; c = c.Parent {
+		if c.N == name {
+			return c.V, c
+		}
+	}
+	return nil, nil
+}
+
+//----------
+
+func (ctx *Ctx) ValueBool(name string) bool {
+	v, _ := ctx.Value(name)
+	if v == nil {
+		return false
+	}
+	return v.(bool)
+}
+
+func (ctx *Ctx) ValueInt(name string) int {
+	v, _ := ctx.Value(name)
+	if v == nil {
+		return 0
+	}
+	return v.(int)
+}
