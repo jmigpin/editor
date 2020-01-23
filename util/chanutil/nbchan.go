b7e7b99ca5cbbbc313ae4a008773bc7949e1c674
Jorge Pinto <jmigpin@gmail.com>
2020-01-23 20:41:56 +0000

    util/chanutil: non-blocking chan utility improvements.

--- util/chanutil/nbchan.go
+++ util/chanutil/nbchan.go
@@ -2,20 +2,30 @@ package chanutil
 
 import (
 	"errors"
+	"fmt"
 	"time"
 )
 
+// Non-blocking channel.
 type NBChan struct {
-	ch chan interface{}
+	ch        chan interface{}
+	LogString string
 }
 
 func NewNBChan() *NBChan {
-	ch := &NBChan{}
-	ch.ch = make(chan interface{})
+	return NewNBChan2(0, "nbchan")
+}
+func NewNBChan2(n int, logS string) *NBChan {
+	ch := &NBChan{
+		ch:        make(chan interface{}, n),
+		LogString: logS,
+	}
 	return ch
 }
 
-// Non-blocking send
+//----------
+
+// Non-blocking send now, or fails with error.
 func (ch *NBChan) Send(v interface{}) error {
 	select {
 	case ch.ch <- v:
@@ -25,18 +35,26 @@ func (ch *NBChan) Send(v interface{}) error {
 	}
 }
 
+// Receives or fails after timeout.
 func (ch *NBChan) Receive(timeout time.Duration) (interface{}, error) {
 	timer := time.NewTimer(timeout)
 	defer timer.Stop()
 
 	select {
 	case <-timer.C:
-		return nil, errors.New("receive timeout")
+		return nil, fmt.Errorf("%v: receive timeout", ch.LogString)
 	case v := <-ch.ch:
 		return v, nil
 	}
 }
 
+//----------
+
 func (ch *NBChan) NewBufChan(n int) {
 	ch.ch = make(chan interface{}, n)
+}
+
+// Setting the channel to zero allows a send to fail immediatly if there is no receiver waiting.
+func (ch *NBChan) SetBufChanToZero() {
+	ch.NewBufChan(0)
 }
