972ddb03b5a2d067b4ce2e4dc67888e07f3495da
Jorge Miguel Pinto <jmigpin@gmail.com>
2021-07-29 22:03:37 +0100

    Fix version to 3.2.1.

--- core/version.go
+++ core/version.go
@@ -2,15 +2,15 @@ package core
 
 func Version() string {
 	// equivalent semantic "go get" version: 1.x.y (z not used)
-	v := "3.2.0"
-	//return versionReleaseCandidate(v)
-	return v
+	v := "3.2.1"
+	return taggedVersion(v)
+	//return v
 }
 
-func versionReleaseCandidate(v string) string {
+func taggedVersion(v string) string {
 	// auto-updated with "go generate" from main directory
-	date := "#___202107271423___#"
-	// release candidate
-	extra := date[4 : len(date)-4]
-	return v + "-rc." + extra
+	date := "#___202107292201___#"
+	tag := date[4 : len(date)-4]
+
+	return v + "-rc." + tag // release candidate
 }
