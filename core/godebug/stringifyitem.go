a89ab13bd0daf66a644997edb2042bcac92e8498
Jorge Miguel Pinto <jmigpin@gmail.com>
2022-04-29 01:58:28 +0100

    godebug: improve stringifyitem (use strings.builder and simplify some output).

--- core/godebug/stringifyitem.go
+++ core/godebug/stringifyitem.go
@@ -3,86 +3,82 @@ package godebug
 import (
 	"fmt"
 	"go/token"
+	"strings"
 
 	"github.com/jmigpin/editor/core/godebug/debug"
 )
 
-//var SimplifyStringifyItem = true
-
 func StringifyItem(item debug.Item) string {
-	is := ItemStringifier{}
+	is := NewItemStringifier()
 	is.stringify(item)
-	return is.Str
+	return is.b.String()
 }
 func StringifyItemFull(item debug.Item) string {
-	is := ItemStringifier{FullStr: true}
+	is := NewItemStringifier()
+	is.fullStr = true
 	is.stringify(item)
-	return is.Str
+	return is.b.String()
 }
-
-// TODO: only option that required is.Str to be updated in order
-//func StringifyItemOffset(item debug.Item, offset int) string {
-//	is := ItemStringifier{Offset: offset}
-//	is.stringify(item)
-//	return is.OffsetValueString
-//}
 
 //----------
 
 type ItemStringifier struct {
-	Str            string
-	FullStr        bool
-	SimplifyResult string
+	b       *strings.Builder
+	fullStr bool
+}
 
-	//Offset            int
-	//OffsetValueString string
+func NewItemStringifier() *ItemStringifier {
+	is := &ItemStringifier{}
+	is.b = &strings.Builder{}
+	return is
+}
+
+func (is *ItemStringifier) p(s string) {
+	is.b.WriteString(s)
 }
 
 //----------
 
-func (is *ItemStringifier) captureStringify(item debug.Item) (start, end int, s string) {
-	start = len(is.Str)
-	is.stringify(item)
-	end = len(is.Str)
-	return start, end, is.Str[start:end]
-}
+//func (is *ItemStringifier) captureStringify(item debug.Item) (start, end int, s string) {
+//	start = len(is.Str)
+//	is.stringify(item)
+//	end = len(is.Str)
+//	return start, end, is.Str[start:end]
+//}
+
+//func (is *ItemStringifier) stringify(item debug.Item) {
+//// capture value
+//start := len(is.Str)
+//defer func() {
+//	end := len(is.Str)
+//	if is.Offset >= start && is.Offset < end {
+//		s := is.Str[start:end]
+//		if is.OffsetValueString == "" || len(s) < len(is.OffsetValueString) {
+//			is.OffsetValueString = s
+//		}
+//	}
+//}()
+
+//is.stringify2(item)
+//}
 
 //----------
 
 func (is *ItemStringifier) stringify(item debug.Item) {
-	//// capture value
-	//start := len(is.Str)
-	//defer func() {
-	//	end := len(is.Str)
-	//	if is.Offset >= start && is.Offset < end {
-	//		s := is.Str[start:end]
-	//		if is.OffsetValueString == "" || len(s) < len(is.OffsetValueString) {
-	//			is.OffsetValueString = s
-	//		}
-	//	}
-	//}()
-
 	is.stringify2(item)
 }
 
 func (is *ItemStringifier) stringify2(item debug.Item) {
-	// NOTE: the string append is done sequentially to allow to detect where the strings are positioned to correctly set "OffsetValueString" if trying to obtain the offset string
+	// NOTE: the string append is done sequentially to allow to detect where the strings are positioned (if later supported)
 
 	//log.Printf("stringifyitem: %T", item)
 
 	switch t := item.(type) {
-	//case string:
-	//	is.Str += t
-	//case int:
-	//	is.Str += fmt.Sprintf("%d", t)
-
 	case *debug.ItemValue:
-		is.FullStr = true
-		if is.FullStr {
-			is.Str += t.Str
+		if is.fullStr {
+			is.p(t.Str)
 		} else {
-			//is.Str += debug.ReducedSprintf(20, "%s", t.Str)
-			is.Str += debug.SprintCutCheckQuote(20, t.Str)
+			is.p(debug.SprintCutCheckQuote(20, t.Str))
 		}
 
 	case *debug.ItemList: // ex: func args list
@@ -91,7 +87,7 @@ func (is *ItemStringifier) stringify2(item debug.Item) {
 		}
 		for i, e := range t.List {
 			if i > 0 {
-				is.Str += ", "
+				is.p(", ")
 			}
 			is.stringify(e)
 		}
@@ -102,155 +98,163 @@ func (is *ItemStringifier) stringify2(item debug.Item) {
 		}
 		for i, e := range t.List {
 			if i > 0 {
-				is.Str += "; "
+				is.p("; ")
 			}
 			is.stringify(e)
 		}
 
 	case *debug.ItemAssign:
-		//if SimplifyStringifyItem {
-		//	is.simplifyItemAssign(t)
-		//} else {
 		is.stringify(t.Lhs)
-		is.Str += " " + token.Token(t.Op).String() + " "
+
+		// it's misleading to get a "2 += 1", better to just show "2 = 1"
+		//is.p(" " + token.Token(t.Op).String() + " ")
+		is.p(" ")
+		switch t2 := token.Token(t.Op); t2 {
+		case token.ADD_ASSIGN, token.SUB_ASSIGN,
+			token.MUL_ASSIGN, token.QUO_ASSIGN,
+			token.REM_ASSIGN,
+			token.INC, token.DEC:
+			is.p("=")
+		default:
+			is.p(t2.String())
+		}
+		is.p(" ")
+
 		is.stringify(t.Rhs)
-		//}
 
 	case *debug.ItemSend:
 		is.stringify(t.Chan)
-		is.Str += " <- "
+		is.p(" <- ")
 		is.stringify(t.Value)
 
 	case *debug.ItemCallEnter:
-		is.Str += "=> "
+		is.p("=> ")
 		is.stringify(t.Fun)
-		is.Str += "("
+		is.p("(")
 		is.stringify(t.Args)
-		is.Str += ")"
+		is.p(")")
 	case *debug.ItemCall:
 		_ = is.result(t.Result)
 		is.stringify(t.Enter.Fun)
-		is.Str += "("
+		is.p("(")
 		is.stringify(t.Enter.Args)
-		is.Str += ")"
+		is.p(")")
 
 	case *debug.ItemIndex:
 		_ = is.result(t.Result)
 		if t.Expr != nil {
 			//switch t2 := t.Expr.(type) {
 			//case string:
-			//	is.Str += t2
+			//	is.p( t2
 			//default:
-			//	is.Str += "("
+			//	is.p( "("
 			//	is.stringify(t.Expr)
-			//	is.Str += ")"
+			//	is.p( ")"
 			//}
 			is.stringify(t.Expr)
 		}
-		is.Str += "["
+		is.p("[")
 		if t.Index != nil {
 			is.stringify(t.Index)
 		}
-		is.Str += "]"
+		is.p("]")
 
 	case *debug.ItemIndex2:
 		_ = is.result(t.Result)
 		if t.Expr != nil {
 			//switch t2 := t.Expr.(type) {
 			//case string:
-			//	is.Str += t2
+			//	is.p( t2
 			//default:
-			//	is.Str += "("
+			//	is.p( "("
 			//	is.stringify(t.Expr)
-			//	is.Str += ")"
+			//	is.p( ")"
 			//}
 			is.stringify(t.Expr)
 		}
-		is.Str += "["
+		is.p("[")
 		if t.Low != nil {
 			is.stringify(t.Low)
 		}
-		is.Str += ":"
+		is.p(":")
 		if t.High != nil {
 			is.stringify(t.High)
 		}
 		if t.Slice3 {
-			is.Str += ":"
+			is.p(":")
 		}
 		if t.Max != nil {
 			is.stringify(t.Max)
 		}
-		is.Str += "]"
+		is.p("]")
 
 	case *debug.ItemKeyValue:
 		is.stringify(t.Key)
-		is.Str += ":"
+		is.p(":")
 		is.stringify(t.Value)
 
 	case *debug.ItemSelector:
-		is.Str += "("
+		is.p("(")
 		is.stringify(t.X)
-		is.Str += ")."
+		is.p(").")
 		is.stringify(t.Sel)
 
 	case *debug.ItemTypeAssert:
 		is.stringify(t.Type)
-		is.Str += "=type("
+		is.p("=type(")
 		is.stringify(t.X)
-		is.Str += ")"
+		is.p(")")
 
 	case *debug.ItemBinary:
 		showRes := is.result(t.Result)
 		if showRes {
-			is.Str += "("
+			is.p("(")
 		}
 		is.stringify(t.X)
-		is.Str += " " + token.Token(t.Op).String() + " "
+		is.p(" " + token.Token(t.Op).String() + " ")
 		is.stringify(t.Y)
 		if showRes {
-			is.Str += ")"
+			is.p(")")
 		}
 
 	case *debug.ItemUnaryEnter:
-		is.Str += "=> "
-		is.Str += token.Token(t.Op).String()
+		is.p("=> ")
+		is.p(token.Token(t.Op).String())
 		is.stringify(t.X)
 	case *debug.ItemUnary:
 		_ = is.result(t.Result)
-		//is.Str += token.Token(t.Op).String()
-		//is.stringify(t.X)
-		is.Str += token.Token(t.Enter.Op).String()
+		is.p(token.Token(t.Enter.Op).String())
 		is.stringify(t.Enter.X)
 
 	case *debug.ItemParen:
-		is.Str += "("
+		is.p("(")
 		is.stringify(t.X)
-		is.Str += ")"
+		is.p(")")
 
 	case *debug.ItemLiteral:
-		is.Str += "{" // other runes: τ, s // ex: A{a:1}, []byte{1,2}
+		is.p("{") // other runes: τ, s // ex: A{a:1}, []byte{1,2}
 		if t != nil {
 			is.stringify(t.Fields)
 		}
-		is.Str += "}"
+		is.p("}")
 
 	case *debug.ItemAnon:
-		is.Str += "_"
+		is.p("_")
 
 	case *debug.ItemBranch:
-		is.Str += "#"
+		is.p("#")
 	case *debug.ItemStep:
-		is.Str += "#"
+		is.p("#")
 	case *debug.ItemLabel:
-		is.Str += "#"
+		is.p("#")
 		if t.Reason != "" {
-			is.Str += " label: " + t.Reason
+			is.p(" label: " + t.Reason)
 		}
 	case *debug.ItemNotAnn:
-		is.Str += fmt.Sprintf("# not annotated: %v", t.Reason)
+		is.p(fmt.Sprintf("# not annotated: %v", t.Reason))
 
 	default:
-		is.Str += fmt.Sprintf("[TODO:(%T)%v]", item, item)
+		is.p(fmt.Sprintf("[TODO:(%T)%v]", item, item))
 	}
 }
 
@@ -266,41 +270,16 @@ func (is *ItemStringifier) result(result debug.Item) bool {
 		isList = true
 	}
 	if isList {
-		is.Str += "("
+		is.p("(")
 	}
 
 	is.stringify(result)
 
 	if isList {
-		is.Str += ")"
+		is.p(")")
 	}
 
-	is.Str += "=" // other runes: ≡
+	is.p("=") // other runes: ≡ // nice, but not all fonts have it defined
 
 	return true
 }
-
-//----------
-
-//func (is *ItemStringifier) simplifyItemAssign(t *debug.ItemAssign) {
-//	s1, e1, str1 := is.captureStringify(t.Lhs)
-//	is.Str += " := "
-//	s2, e2, str2 := is.captureStringify(t.Rhs)
-//	_, _, _, _ = s1, e1, s2, e2
-
-//	// remove repeated results
-//	w := []string{str1 + "=", "(" + str1 + ")="}
-//	for _, s := range w {
-//		if strings.HasPrefix(str2, s) {
-//			is.Str = is.Str[:s2] + is.Str[s2+len(s):]
-//			return
-//		}
-//	}
-
-//	// also removes ":="
-//	s := str1
-//	if strings.HasPrefix(str2, s) {
-//		is.Str = is.Str[:e1] + is.Str[s2+len(s):]
-//		return
-//	}
-//}
