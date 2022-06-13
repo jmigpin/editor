fea321b390de837b1a32e9d2b56fbbb16a2e90d1
Jorge Miguel Pinto <jmigpin@gmail.com>
2022-06-13 17:37:45 +0100

    Bump alpha version.

--- core/version.go
+++ core/version.go
@@ -7,21 +7,32 @@ import (
 
 func Version() string {
 	// NOTE: equivalent "go get" version in format x.y.z is 1.x.y (z not used) (ex: 3.3.0 -> 1.3.3). This is done because go doesn't seem to allow having versions bigger then 1 without altering the import paths.
-	v := "3.3.0"
-	return timedVersion(v, true)
+	v := "3.4.0"
+	//typ := "release"
+	//typ:="rc" // release candidate
+	typ := "alpha" // development
+	return timedVersion(v, typ)
 }
-func timedVersion(v string, release bool) string {
+func timedVersion(v string, typ string) string {
+	// TODO: build-metadata (possibly from OS, git hash, ...)
+
 	t := versionTime()
-	if !release {
-		tag := t.Format("200601021504")
-		return fmt.Sprintf("%s-rc.%s", v, tag) // release-candidate format
-		//return fmt.Sprintf("%s+%s", tag) // build-metadata format (ignored to determine version precedence)
+	tagt := t.Format("200601021504")
+	switch typ {
+	case "alpha":
+		return fmt.Sprintf("%s-alpha.%s", v, tagt)
+	case "rc":
+		return fmt.Sprintf("%s-rc.%s", v, tagt)
+	case "release":
+		// "+" is build-metadata; ignored to determine version precedence
+		return fmt.Sprintf("%s+%s", v, tagt)
+	default:
+		panic("!")
 	}
-	return fmt.Sprintf("%s (%s)", v, t.Format("2006/01/02 15:04"))
 }
 func versionTime() time.Time {
 	// auto-updated with "go generate" from main directory
-	date := "#___202206131603___#"
+	date := "#___202206131724___#"
 	tag := date[4 : len(date)-4]
 
 	layout := "200601021504"
