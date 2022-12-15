59ac3262a2db52acde8a70dc96cc30d9a813c0c8
Jorge Miguel Pinto <jmigpin@gmail.com>
2022-12-15 00:29:57 +0000

    Bump alpha version.

--- core/version.go
+++ core/version.go
@@ -9,10 +9,10 @@ import (
 func Version() string {
 	// NOTE: equivalent "go get" version in format x.y.z is 1.x.y (z not used) (ex: 3.3.0 -> 1.3.3). This is done because go doesn't seem to allow having versions bigger then 1 without altering the import paths.
 
-	v := "3.6.0"
-	typ := "release"
+	v := "3.7.0"
+	//typ := "release"
 	//typ := "rc" // release candidate
-	//typ := "alpha" // development
+	typ := "alpha" // development
 	return taggedVersion(v, typ)
 }
 func taggedVersion(v string, typ string) string {
