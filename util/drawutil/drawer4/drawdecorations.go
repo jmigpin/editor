99a2ad8ddd51ed18848c81aecd8c71299502519e
Jorge Miguel Pinto <jmigpin@gmail.com>
2026-04-11 03:14:56 +0100

    util/drawutil/drawer4: draw terminal horizontal rules in final pass

--- util/drawutil/drawer4/drawdecorations.go
+++ util/drawutil/drawer4/drawdecorations.go
@@ -2,6 +2,7 @@ package drawer4
 
 import (
 	"image"
+	"image/draw"
 
 	"github.com/jmigpin/editor/util/imageutil"
 )
@@ -13,67 +14,51 @@ type DrawDecorations struct {
 func (dd *DrawDecorations) Init() {}
 
 func (dd *DrawDecorations) Iter() {
-	dd.draw()
 	if !dd.d.iterNext() {
 		return
 	}
 }
 
-func (dd *DrawDecorations) End() {}
-
-func (dd *DrawDecorations) draw() {
-	dec := dd.activeDecoration()
-	if dec == nil || dec.Kind != DecorationHorizontalRule || dec.Fg == nil {
-		return
-	}
-
-	r := dd.d.iters.runeR.penBoundsRect()
-	b := dd.d.bounds
-	th := dec.Thickness
-	if th <= 0 {
-		th = max(1, r.Dy()/12)
-	}
-	y := r.Min.Y - th/2
-
-	adv := dd.d.iters.runeR.glyphAdvance('W').Ceil()
-	if adv <= 0 {
-		adv = 8
-	}
-	dashW := adv
-	gapW := adv
-	for x := b.Min.X; x < b.Max.X; x += dashW + gapW {
-		r2 := image.Rect(x, y, min(x+dashW, b.Max.X), y+th)
-		r2 = r2.Intersect(b)
-		imageutil.FillRectangle(dd.d.st.drawR.img, r2, dec.Fg)
-	}
-}
-
-func (dd *DrawDecorations) activeDecoration() *Decoration {
-	if !dd.d.iters.runeR.isNormal() {
-		return nil
-	}
-	ri := dd.d.st.runeR.ri
-	for gi, g := range dd.d.Opt.Decorations.Groups {
+func (dd *DrawDecorations) End() {
+	img := dd.d.st.drawR.img
+	for _, g := range dd.d.Opt.Decorations.Groups {
 		if g == nil || g.Off {
 			continue
 		}
-		i := &dd.d.st.decorations.indexes[gi]
-		for ; *i < len(g.Entries); *i++ {
-			entry := g.Entries[*i]
-			if entry == nil {
+		for _, dec := range g.Entries {
+			if dec == nil {
 				continue
 			}
-			if !dd.d.iters.decorations.isValidLineStartOffset(entry.Offset) {
-				continue
-			}
-			if entry.Offset < ri {
-				continue
-			}
-			if entry.Offset > ri {
-				break
-			}
-			return entry
+			dd.draw(img, dec)
 		}
 	}
-	return nil
+}
+
+func (dd *DrawDecorations) draw(img draw.Image, dec *Decoration) {
+	if dec.Kind != DecorationHorizontalRule || dec.Fg == nil {
+		return
+	}
+
+	p := dd.d.LocalPointOf(dec.Offset)
+	b := dd.d.bounds
+	th := dec.Thickness
+	if th <= 0 {
+		th = max(1, dd.d.fface.LineHeightInt()/12)
+	}
+	y := p.Y - th/2
+
+	adv, ok := dd.d.fface.Face.GlyphAdvance('W')
+	if !ok {
+		adv = 0
+	}
+	dashW := adv.Ceil()
+	if dashW <= 0 {
+		dashW = 8
+	}
+	gapW := dashW
+	for x := b.Min.X; x < b.Max.X; x += dashW + gapW {
+		r2 := image.Rect(x, y, min(x+dashW, b.Max.X), y+th)
+		r2 = r2.Intersect(b)
+		imageutil.FillRectangle(img, r2, dec.Fg)
+	}
 }
