570362b8232289b2355a01b7c4555da6d9335ef0
Jorge Miguel Pinto <jmigpin@gmail.com>
2026-04-18 06:30:56 +0100

    sampleparsers: call json parsers explicitly

--- util/parseutil/sampleparsers/jsonparser_test.go
+++ util/parseutil/sampleparsers/jsonparser_test.go
@@ -4,13 +4,15 @@ package sampleparsers
 ////godebug:annotatepackage:github.com/jmigpin/editor/util/parseutil
 
 import (
+	"encoding/json"
+	"reflect"
 	"testing"
 )
 
 func TestJsonParser(t *testing.T) {
 	//s := "{\"a\":{}}"
 	s := jsonparserInput1
-	v, err := ParseJson([]byte(s))
+	v, err := ParseJson3([]byte(s))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -21,22 +23,64 @@ func TestJsonParser(t *testing.T) {
 }
 
 //----------
+//----------
+//----------
 
-func ParseJson(src []byte) (any, error) {
-	//p := NewJsonParser(src)
-	//return p.parseJson()
-	p := NewJsonParser2()
-	return p.parseJson(src)
+func TestJsonParser3CompareEncodingJson(t *testing.T) {
+	src := []byte(jsonparserInput1)
+
+	v1, err := ParseJson3(src)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	v2 := any(nil)
+	if err := json.Unmarshal(src, &v2); err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(v1, v2) {
+		t.Fatalf("values differ\nbtparser: %#v\nencoding/json: %#v", v1, v2)
+	}
 }
 
-//----------
-//----------
-//----------
+func TestJsonParser2And3Parse(t *testing.T) {
+	src := []byte(jsonparserInput1)
+
+	if _, err := ParseJson2(src); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := ParseJson3(src); err != nil {
+		t.Fatal(err)
+	}
+}
 
 func BenchmarkJsonParser(b *testing.B) {
 	s := jsonparserInput1
 	for i := 0; i < b.N; i++ {
-		v, err := ParseJson([]byte(s))
+		v, err := ParseJson3([]byte(s))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = v
+	}
+}
+
+func BenchmarkJsonParser2(b *testing.B) {
+	s := jsonparserInput1
+	for i := 0; i < b.N; i++ {
+		v, err := ParseJson2([]byte(s))
+		if err != nil {
+			b.Fatal(err)
+		}
+		_ = v
+	}
+}
+
+func BenchmarkJsonParser3(b *testing.B) {
+	s := jsonparserInput1
+	for i := 0; i < b.N; i++ {
+		v, err := ParseJson3([]byte(s))
 		if err != nil {
 			b.Fatal(err)
 		}
